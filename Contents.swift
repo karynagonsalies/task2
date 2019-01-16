99841162ec9290fc3ad2183c8c3b675662c0f893
karynagonsalies <46356577+karynagonsalies@users.noreply.github.com>
2019-01-16 21:32:15 +0100

    remove description

--- Contents.swift
+++ Contents.swift
@@ -1,12 +1,5 @@
 import UIKit
 
-//Zadanie nr 2 - Konta bankowe
-//1. Stwórz metodę, która przyjmuje numer konta jako string i zwraca obiekt typu BankAccount, który zawiera: numer konta, identyfikator banku, nazwę banku,
-//2. jeśli numer konta nie wskazuje na żaden bank zwróć nil,
-//3. dodatkowo metoda powinna sprawdzać czy liczba cyfr w numerze konta jest prawidłowa,
-//4. wszystkie informacje odnośnie procesu identyfikacji banku znajdziesz pod adresem: https://
-//www.najlepszekonto.pl/identyfikacja-banku-po-numerze-konta
-
 let dictionaryBank = [
     "1010":    "Narodowy Bank Polski",
     "1020":    "PKO BP",
